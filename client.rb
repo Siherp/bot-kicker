22768570ed60fc2ce9ab2f1efd5d95df05a0eccc
Jason Carter <jasonwcarter@outlook.com>
2015-09-15 19:59:05 -0600

    Adds readme, .env.sample, and changes up the client a tad.

--- client.rb
+++ client.rb
@@ -7,22 +7,19 @@ Slack.configure do |config|
 end
 
 client = Slack::RealTime::Client.new
+puts "Starting kicker-bot"
 
 client.on :message do |data|
-  kick_user(client, data) if data['channel'] == ENV['JSONTESTING'] && data['subtype'] == 'group_join' && data['user'] == ENV['PHIL']
-end
-
-def countdown(client)
-  client.web_client.chat_postMessage({channel: ENV['JSONTESTING'], text: 'Kicking him in: '})
-  (1..3).to_a.reverse.each do |num|
-    client.web_client.chat_postMessage({channel: ENV['JSONTESTING'], text: num.to_s })
-    sleep(1)
-  end
+  kick_user(client, data) if data['channel'] == ENV['CHANNEL'] && data['subtype'] == 'group_join' && data['user'] == ENV['USER']
 end
 
 def kick_user(client, data)
-  client.web_client.chat_postMessage({channel: ENV['JSONTESTING'], text: "I don't think <@#{data['user']}> wants to do pushups anymore."})
-  countdown(client)
+  client.web_client.chat_postMessage({channel: ENV['CHANNEL'], text: "I don't think <@#{data['user']}> wants to do pushups anymore."})
+  client.web_client.chat_postMessage({channel: ENV['CHANNEL'], text: 'Kicking him in: '})
+  (1..3).to_a.reverse.each do |num|
+    client.web_client.chat_postMessage({channel: ENV['CHANNEL'], text: num.to_s })
+    sleep(1)
+  end
   client.web_client.groups_kick({channel: data['channel'], user: data['user']})
 end
 
